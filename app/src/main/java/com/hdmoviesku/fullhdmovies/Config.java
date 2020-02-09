efc9ac0a4a348b5786bc152be343ccf0ac09e414
fando <fandofast@gmail.com>
2020-02-09 16:23:43 +0700

    Initial commit

---
+++ app/src/main/java/com/hdmoviesku/fullhdmovies/Config.java
@@ -0,0 +1,20 @@
+package com.hdmoviesku.fullhdmovies;
+
+
+
+public class Config {
+
+    // copy your api url from php admin dashboard & paste below
+    public static final String API_SERVER_URL = "https://movie1.hdmoviesku.xyz/api/";
+
+    //copy your api key from php admin dashboard & paste below
+    public static final String API_KEY = "lovb6y5k3vv4soq5e7qfpghr";
+
+    //copy your terms url from php admin dashboard & paste below
+    public static final String TERMS_URL = "https://movie1.hdmoviesku.xyz/api/terms/";
+
+    //default theme
+    public static boolean DEFAULT_DARK_THEME_ENABLE = false;
+
+
+}
---
+++ app/src/main/java/com/hdmoviesku/fullhdmovies/fullhdmovies_network/apis/SearchApi.java
@@ -0,0 +1,15 @@
+package com.hdmoviesku.fullhdmovies.fullhdmovies_network.apis;
+
+import com.hdmoviesku.fullhdmovies.fullhdmovies_network.model.SearchModel;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface SearchApi {
+
+    @GET("search")
+    Call<SearchModel> getSearchData(@Query("api_secret_key") String apiSecreteKey,
+                                    @Query("q") String query);
+
+}
